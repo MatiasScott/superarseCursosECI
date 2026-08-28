e63066cc8506ed63f216898a388fb7559600b386
root <root@srv1747580.hstgr.cloud>
2026-08-28 14:47:24 +0000

    Configurar Docker y variables de entorno

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,11 @@
 <?php
-// Datos de la base de datos
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'superar1_sistema_educacion_continua');
-define('DB_USER', 'superar1_Tics'); // Tu usuario de MySQL
-define('DB_PASS', '/Msvs5297*');     // Tu contraseña de MySQL
 
-// Configuración de PayPhone
-// Token de PayPhone (Credenciales de tu ejemplo)
-define('PAYPHONE_TOKEN', 'Z4V-DuSKF-391teDlBMmqxN2I7AJIvwcSTqkkGRYu18qxmJdIm8MIz9NIRJNSviO0pp7tutRbWR6lC2SV3YJBbp23thUHlugAyDJDF8udWiK1T58u0n0HAOnN6q5X0WBHCppAATSxA6dy1WbXW4UywVwW4nhzyWqiEpeY3AhddyhCzy6yCmjLFddSjps50gpLf5CR2gaqblLRjgz5S2LlW_dqnj6vpSSZFQnx_lw3JnieqDelvcM4IRyas0zxqq9cogJg2QazbY7TQqPqGGfxCDo5uN-o-ePJN2MXipu8Ua1BQZEOl7P92-UVfIN34vSQYHYI9cQ9IezaWLiD9L-KSb3Bic');
+// Base de datos
+define('DB_HOST', getenv('DB_HOST') ?: 'mariadb');
+define('DB_NAME', getenv('DB_NAME') ?: 'superar1_sistema_educacion_continua');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
 
-// Store ID de PayPhone
-define('PAYPHONE_STORE_ID', 'd3fcb722-dfe9-4e7c-8b33-cf8fbd309006');
+// PayPhone
+define('PAYPHONE_TOKEN', getenv('PAYPHONE_TOKEN') ?: '');
+define('PAYPHONE_STORE_ID', getenv('PAYPHONE_STORE_ID') ?: '');
